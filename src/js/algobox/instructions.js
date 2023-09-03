71a61d2d7951831787a15ec674d863f7c910da3d
pjehan <pierre.jehan@gmail.com>
2023-09-03 10:35:28 +0200

    Add new slides about variables

--- src/js/algobox/instructions.js
+++ src/js/algobox/instructions.js
@@ -11,32 +11,32 @@ export function displayInstructionComment(node, container) {
 }
 
 export function displayInstructionSetVariable(node, container) {
-  const text = node.getAttribute('algoitem');
-  displayVariable(text.split(' ')[0], container);
-  displayKeyword(text.split(' ')[1], container);
-  displayValue(text.split(' ')[2], container);
+  const text = node.getAttribute('algoitem').split(' ');
+  displayVariable(text.shift(), container);
+  displayKeyword(text.shift(), container);
+  displayValue(text.join(' '), container);
 }
 
 export function displayInstructionReadVariable(node, container) {
-  const text = node.getAttribute('algoitem');
-  displayKeyword(text.split(' ')[0], container);
-  displayVariable(text.split(' ')[1], container);
+  const text = node.getAttribute('algoitem').split(' ');
+  displayKeyword(text.shift(), container);
+  displayVariable(text.join(' '), container);
 }
 
 export function displayInstructionDisplayVariable(node, container) {
-  const text = node.getAttribute('algoitem');
-  displayKeyword(text.split(' ')[0], container);
-  displayVariable(text.split(' ')[1], container);
+  const text = node.getAttribute('algoitem').split(' ');
+  displayKeyword(text.shift(), container);
+  displayVariable(text.join(' '), container);
 }
 
 export function displayInstructionDisplayText(node, container) {
-  const text = node.getAttribute('algoitem');
-  displayKeyword(text.split(' ')[0], container);
-  displayText(text.split(' ')[1], container);
+  const text = node.getAttribute('algoitem').split(' ');
+  displayKeyword(text.shift(), container);
+  displayText(text.join(' '), container);
 }
 
 export function displayInstructionCalculation(node, container) {
-  const text = node.getAttribute('algoitem');
-  displayKeyword(text.split(' ')[0], container);
-  displayText(text.split(' ')[1], container);
+  const text = node.getAttribute('algoitem').split(' ');
+  displayKeyword(text.shift(), container);
+  displayText(text.join(' '), container);
 }
